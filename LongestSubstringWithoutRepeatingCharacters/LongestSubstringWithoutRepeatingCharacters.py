1caf575b2f283feebc5484635a0465aa9371cc3b
abLife <jianlingzh@gmail.com>
2015-07-07 21:30:40 -0400

    add LongestSubstringWithoutRepeatingCharacters - an inefficient solution

---
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,43 @@
+
+
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def lengthOfLongestSubstring(self, s):
+        ''' a really inefficient one using two pointers'''
+        
+        if len(s) == 0:
+            return 0
+        
+        if len(s) == 1:
+            return 1
+        
+        if len(s) == 2:
+            if s[0] != s[1]:
+                return 2
+            else:
+                return 1
+            
+        #use two pointers
+        start = 0
+        longest = 1
+        d = {}
+        d[s[0]] = 0
+        i = 1
+        
+        while i < len(s):
+            if s[i] in d:
+                if i - start > longest:
+                    longest = i - start
+                start = d[s[i]] + 1
+                i = start
+                d = {}
+            
+            d[s[i]] = i
+            i += 1
+        
+        if i - start > longest:
+            longest = i - start
+            
+        return longest
+        
